3b7c40faaea6a497482001bb3a2a3893f923ccd4
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2025-03-13 22:21:28 +0400

    Changed File and Folder models to cascade upon delete

---
+++ prisma/migrations/20250313182046_cascade_added_and_removed_bytes/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `content` on the `File` table. All the data in the column will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "File" DROP CONSTRAINT "File_folderId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Folder" DROP CONSTRAINT "Folder_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "File" DROP COLUMN "content";
+
+-- AddForeignKey
+ALTER TABLE "Folder" ADD CONSTRAINT "Folder_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_folderId_fkey" FOREIGN KEY ("folderId") REFERENCES "Folder"("id") ON DELETE CASCADE ON UPDATE CASCADE;
