4a8434413f400b2d526b8aeb74ac38f02ca0d804
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2025-03-14 20:51:04 +0400

    Partial feature scaffold: file breakdown and viewing

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,8 +1,17 @@
 const links = require("../links");
 const db = require("../db");
 
-module.exports.getFile = (req, res) => {
-  const title = "Viewing ";
+module.exports.getFile = async (req, res) => {
+  const { fileId, fileName } = req.params;
+  const title = "Viewing " + fileName;
   const location = "/file";
-  res.send("TODO");
+  const file = await db.file.findFirst({
+    where: {
+      id: fileId,
+      name: fileName,
+    },
+  });
+
+  console.dir(file, null);
+  res.render("pages/file", { links, title, location, file });
 };
